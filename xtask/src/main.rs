b34339799d2ed1ace5dab9df7d8d87d87a592a3b
JustinBacher <92jbach@gmail.com>
2024-11-17 14:06:45 -0500

    got a working man page generator

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -4,42 +4,32 @@ extern crate clap_mangen;
 extern crate clippy;
 extern crate itertools;
 
-use std::{env, fs::write};
+use std::{env, fs::write, process::exit};
 
 use clap::CommandFactory;
-use clap_complete::Shell;
+use clap_complete::Shell::*;
 use clap_mangen::Man;
-use itertools::Either;
-
 use clippy::{cli::ClippyCli, commands::completions::write_to_config, prelude::Result};
+use itertools::Either::Right;
 
 fn main() -> Result<()> {
     if let Err(e) = try_main() {
         eprintln!("{}", e);
-        std::process::exit(-1);
+        exit(-1);
     }
-
     Ok(())
 }
 
 fn try_main() -> Result<()> {
-    let task = env::args().nth(1);
-
-    match task.as_deref() {
+    match env::args().nth(1).as_deref() {
         Some("mangen") => man_gen()?,
         Some("completions") => {
-            for shell in [Shell::Bash, Shell::Zsh, Shell::Fish] {
-                let out_dir = env!("CARGO_MANIFEST_DIR");
-
-                write_to_config(
-                    shell,
-                    &mut ClippyCli::command(),
-                    Either::Right(out_dir.to_string()),
-                )
-                .unwrap();
+            let out_dir = env!("CARGO_MANIFEST_DIR");
+            for shell in [Bash, Zsh, Fish] {
+                write_to_config(shell, &mut ClippyCli::command(), Right(out_dir.to_string()))?;
             }
         },
-        _ => return Ok(()),
+        _ => panic!("Invalid argument passed"),
     }
     Ok(())
 }
@@ -50,8 +40,5 @@ fn man_gen() -> Result<()> {
     let mut buffer: Vec<u8> = Default::default();
     man.render(&mut buffer)?;
 
-    dbg!(out_dir);
-    write(out_dir, buffer)?;
-
-    Ok(())
+    Ok(write(out_dir, buffer)?)
 }
