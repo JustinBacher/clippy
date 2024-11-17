b1eade1b95da5648aa87fbbd043a2333fdad970d
JustinBacher <92jbach@gmail.com>
2024-11-17 14:05:49 -0500

    don't need verbose in check

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cargo check --verbose
-cargo +nightly fmt --all -- --check --verbose
-cargo test --verbose
+cargo +nightly fmt --all
+cargo check
+cargo test
